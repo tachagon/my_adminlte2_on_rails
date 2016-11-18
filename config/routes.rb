25b3c103b82518b38fb7cdd60ad042c403fb377b
Tatchagon <o_k_t@hotmail.com>
2016-11-18 14:42:49 +0700

    adminlte theme for ruby on rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#index'
 end
